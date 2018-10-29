7b218cd5cdc16219843f54ba67e634567142028f
LelandYan <2721599586@qq.com>
2018-10-29 23:48:52 +0800

    建立神经网络

---
+++ 建立神将网络.py
@@ -0,0 +1,17 @@
+# _*_ coding: utf-8 _*_
+__author__ = 'LelandYan'
+__date__ = '2018/10/29 23:42'
+
+import tensorflow as tf
+import numpy as np
+
+
+def add_layer(inputs, in_size, out_size, activation_function=None):
+    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
+    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
+    Wx_plus_b = tf.matmul(inputs, Weights) + biases
+    if activation_function is None:
+        outputs = Wx_plus_b
+    else:
+        outputs = activation_function(Wx_plus_b)
+    return outputs
