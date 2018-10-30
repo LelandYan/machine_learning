9089bc1ee3afbed996b74bd8e51a081c7b29f23f
LelandYan <2721599586@qq.com>
2018-10-30 09:33:36 +0800

    tensorborad使用

--- 关于运行tensorboard方法以及问题解决.md
+++ 关于运行tensorboard方法以及问题解决.md
@@ -1,4 +1,4 @@
 ###tensorboard
 1. 打开命令提示符并进入安装tensorflow的环境中如果使用的是Anaconda，打开Anaconda Prompt后输入
-"activate 你的环境名"，然后在进入你项目的所在的盘符，让后输入"tensorboard --logdir=='你的可视化的文件目录’ 
---host 127.0.0.1",回车，输入网址，然后就可以运行了
+"activate 你的环境名"，然后在进入你的生成图文件夹所在的父目录，让后输入
+"tensorboard --logdir=你的可视化的文件目录 --host=127.0.0.1",回车，输入网址，然后就可以运行了
--- 建立神将网络.py
+++ 建立神将网络.py
@@ -8,31 +8,39 @@ import matplotlib.pyplot as plt
 
 
 def add_layer(inputs, in_size, out_size, activation_function=None):
-    Weights = tf.Variable(tf.random_normal([in_size, out_size]))
-    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
-    Wx_plus_b = tf.matmul(inputs, Weights) + biases
-    if activation_function is None:
-        outputs = Wx_plus_b
-    else:
-        outputs = activation_function(Wx_plus_b)
-    return outputs
+    with tf.name_scope('layer'):
+        with tf.name_scope("weights"):
+            Weights = tf.Variable(tf.random_normal([in_size, out_size]),name='w')
+        with tf.name_scope("biases"):
+            biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
+        with tf.name_scope('Wx_plus_b'):
+            Wx_plus_b = tf.matmul(inputs, Weights) + biases
+        if activation_function is None:
+            outputs = Wx_plus_b
+        else:
+            outputs = activation_function(Wx_plus_b)
+        return outputs
 
 
 x_data = np.linspace(-1, 1, 300)[:, np.newaxis]
 noise = np.random.normal(0, 0.05, x_data.shape)
 y_data = np.square(x_data) - 0.5 + noise
 
-xs = tf.placeholder(tf.float32, [None, 1])
-ys = tf.placeholder(tf.float32, [None, 1])
+with tf.name_scope("inputs"):
+    xs = tf.placeholder(tf.float32, [None, 1],name='x_input')
+    ys = tf.placeholder(tf.float32, [None, 1],name='y_input')
 
 l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
 prediction = add_layer(l1, 10, 1, activation_function=None)
 
-loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices=[1]))
-train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+with tf.name_scope('loss'):
+    loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys - prediction), reduction_indices=[1]))
+with tf.name_scope('train'):
+    train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
 init = tf.initialize_all_variables()
 with tf.Session() as sess:
+    writer = tf.summary.FileWriter(r'E:\tensorflow_learning\logs', sess.graph)
     sess.run(init)
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
@@ -48,5 +56,6 @@ with tf.Session() as sess:
                 pass
             prediction_value = sess.run(prediction, feed_dict={xs: x_data})
             lines = ax.plot(x_data, prediction_value, 'r-', lw=5)
-            plt.pause(0.1)
+            plt.pause(0.05)
+    plt.pause(1000)
         #     print(sess.run(loss,feed_dict={xs:x_data,ys:y_data}))
