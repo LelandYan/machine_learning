83769e64a7355be5f7352ccfe17c0f1a6c7c65bf
LelandYan <2721599586@qq.com>
2018-11-03 00:14:34 +0800

    rnn

---
+++ numpy_learning/numpy_test1.py
@@ -0,0 +1,11 @@
+# _*_ coding: utf-8 _*_
+__author__ = 'LelandYan'
+__date__ = '2018/11/3 0:09'
+
+import numpy as np
+
+array = np.array([[1,2,3],[2,3,4]])
+print(array)
+print(array.ndim)
+print(array.shape)
+print(array.size)
