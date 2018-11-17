3855cabd069236c5f05fe6f55fe00a53a2bf1a45
LelandYan <2721599586@qq.com>
2018-11-17 20:04:53 +0800

    project

--- 疾病预测/select_features.py
+++ 疾病预测/select_features.py
@@ -1,4 +1,4 @@
-# _*_ coding: utf-8 _*_
+ # _*_ coding: utf-8 _*_
 __author__ = 'LelandYan'
 __date__ = '2018/11/15 0:15'
 import numpy as np
---
+++ 疾病预测/train_model.py
@@ -0,0 +1,8 @@
+# _*_ coding: utf-8 _*_
+__author__ = 'LelandYan'
+__date__ = '2018/11/17 20:03'
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
