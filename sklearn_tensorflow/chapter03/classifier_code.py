e3eee485e7f323d6482a1e6084772df7750d7cdf
LelandYan <2721599586@qq.com>
2019-03-09 13:28:11 +0800

    score

--- sklearn_tensorflow/chapter03/classifier_code.py
+++ sklearn_tensorflow/chapter03/classifier_code.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import os
 import matplotlib as mpl
 
+np.random.seed(42)
 # to plot the pretty figure
 mpl.rc("axes", labelsize=14)
 mpl.rc("xtick", labelsize=12)
@@ -36,9 +37,7 @@ def plot_digit(data):
     image = data.reshape(28,28)
     plt.imshow(image,cmap=mpl.cm.binary,interpolation="nearest")
     plt.axis("off")
-# some_digit = X[36000]
-# save_fig("some_digit_plot")
-# plt.show()
+
 def plot_digits(instances,images_per_row=10,**options):
     size = 28
     images_per_row = min(len(instances),images_per_row)
@@ -54,8 +53,28 @@ def plot_digits(instances,images_per_row=10,**options):
     plt.imshow(image,cmap=mpl.cm.binary,**options)
     plt.axis("off")
 
-plt.figure(figsize=(9,9))
-example_images = np.r_[X[:12000:600],X[13000:30600:600],X[30600:60000:590]]
-plot_digits(example_images,images_per_row=10)
-save_fig("more_digits_plot")
-plt.show()
+# plt.figure(figsize=(9,9))
+# example_images = np.r_[X[:12000:600],X[13000:30600:600],X[30600:60000:590]]
+# plot_digits(example_images,images_per_row=10)
+# save_fig("more_digits_plot")
+# plt.show()
+
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+
+shuffle_index = np.random.permutation(60000)
+X_train,y_train= X_train[shuffle_index],y_train[shuffle_index]
+
+# Binary classifier
+y_train_5 = (y_train == 5)
+y_test_5 = (y_test == 5)
+
+some_digit = X[36000]
+from sklearn.linear_model import SGDClassifier
+sgd_clf = SGDClassifier(max_iter=5,tol=np.infty,random_state=42)
+(sgd_clf.fit(X_train,y_train_5))
+sgd_clf.predict([some_digit])
+
+# 交叉验证
+from sklearn.model_selection import cross_val_score
+res = cross_val_score(sgd_clf,X_train,y_train_5,cv=3,scoring="accuracy")
+print(res)
