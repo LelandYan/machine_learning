6da45de6ebd8efb3ee7d12d122232021104fc7c8
LelandYan <2721599586@qq.com>
2019-03-15 13:12:56 +0800

    gb

--- sklearn_tensorflow/chapter07/voting.py
+++ sklearn_tensorflow/chapter07/voting.py
@@ -49,9 +49,18 @@ def plot_decision_boundary(clf, X, y, axes=[-1.5, 2.5, -1, 1.5], alpha=0.5, cont
     plt.xlabel(r"$x_1$", fontsize=18)
     plt.ylabel(r"$x_2$", fontsize=18, rotation=0)
 
-def plot_predictions(regressors,X,y,axes,label=None,style="r-",data_style="b.",data_label=None):
-    x1 = np.linspace(axes[0],axes[1],500)
-    y_pred = sum(regressors.predict(x1.reshape(-1,1)) for regressors in regressors)
+
+def plot_predictions(regressors, X, y, axes, label=None, style="r-", data_style="b.", data_label=None):
+    x1 = np.linspace(axes[0], axes[1], 500)
+    # 这里是生成的是一个迭代器，sum的作用是将迭代器转化为值
+    y_pred = sum(regressors.predict(x1.reshape(-1, 1)) for regressors in regressors)
+    plt.plot(X[:, 0], y, data_style, label=data_label)
+    plt.plot(x1, y_pred, style, linewidth=2, label=label)
+    if label or data_label:
+        plt.legend(loc="upper center", fontsize=16)
+    plt.axis(axes)
+
+
 if __name__ == '__main__':
     # 采用500决策树集成
     # bag_clf = BaggingClassifier(DecisionTreeClassifier(splitter="random",max_leaf_nodes=16,random_state=42),
@@ -120,23 +129,91 @@ if __name__ == '__main__':
     #
     #     plt.show()
 
-
     # Gradient Boosting
-    np.random.seed(42)
+    # np.random.seed(42)
+    #     # X = np.random.rand(100, 1) - 0.5
+    #     # y = 3 * X[:, 0] ** 2 + 0.05 * np.random.randn(100)
+    #     #
+    #     # from sklearn.tree import DecisionTreeRegressor
+    #     #
+    #     # tree_reg1 = DecisionTreeRegressor(max_depth=2,random_state=42)
+    #     # tree_reg1.fit(X,y)
+    #     #
+    #     # y2 = y - tree_reg1.predict(X)
+    #     # tree_reg2 = DecisionTreeRegressor(max_depth=2,random_state=42)
+    #     #
+    #     # tree_reg2.fit(X,y2)
+    #     # y3 = y2 - tree_reg2.predict(X)
+    #     # tree_reg3 = DecisionTreeRegressor(max_depth=2,random_state=42)
+    #     #
+    #     # tree_reg3.fit(X,y3)
+    #     # X_new = np.array([[0.8]])
+    #     # y_pred = sum(tree.predict(X_new) for tree in (tree_reg1, tree_reg2, tree_reg3))
+    #     #
+    #     # plt.figure(figsize=(11,11))
+    #     # plt.subplot(321)
+    #     # plot_predictions([tree_reg1], X, y, axes=[-0.5, 0.5, -0.1, 0.8], label="$h_1(x_1)$", style="g-",
+    #     #                  data_label="Training set")
+    #     # plt.ylabel("$y$", fontsize=16, rotation=0)
+    #     # plt.title("Residuals and tree predictions", fontsize=16)
+    #     #
+    #     # plt.subplot(322)
+    #     # plot_predictions([tree_reg1], X, y, axes=[-0.5, 0.5, -0.1, 0.8], label="$h(x_1) = h_1(x_1)$",
+    #     #                  data_label="Training set")
+    #     # plt.ylabel("$y$", fontsize=16, rotation=0)
+    #     # plt.title("Ensemble predictions", fontsize=16)
+    #     #
+    #     # plt.subplot(323)
+    #     # plot_predictions([tree_reg2], X, y2, axes=[-0.5, 0.5, -0.5, 0.5], label="$h_2(x_1)$", style="g-", data_style="k+",
+    #     #                  data_label="Residuals")
+    #     # plt.ylabel("$y - h_1(x_1)$", fontsize=16)
+    #     #
+    #     # plt.subplot(324)
+    #     # plot_predictions([tree_reg1, tree_reg2], X, y, axes=[-0.5, 0.5, -0.1, 0.8], label="$h(x_1) = h_1(x_1) + h_2(x_1)$")
+    #     # plt.ylabel("$y$", fontsize=16, rotation=0)
+    #     #
+    #     # plt.subplot(325)
+    #     # plot_predictions([tree_reg3], X, y3, axes=[-0.5, 0.5, -0.5, 0.5], label="$h_3(x_1)$", style="g-", data_style="k+")
+    #     # plt.ylabel("$y - h_1(x_1) - h_2(x_1)$", fontsize=16)
+    #     # plt.xlabel("$x_1$", fontsize=16)
+    #     #
+    #     # plt.subplot(326)
+    #     # plot_predictions([tree_reg1, tree_reg2, tree_reg3], X, y, axes=[-0.5, 0.5, -0.1, 0.8],
+    #     #                  label="$h(x_1) = h_1(x_1) + h_2(x_1) + h_3(x_1)$")
+    #     # plt.xlabel("$x_1$", fontsize=16)
+    #     # plt.ylabel("$y$", fontsize=16, rotation=0)
+    #     #
+    #     # plt.show()
+    import numpy as np
+    from sklearn.model_selection import train_test_split
+    from sklearn.metrics import mean_squared_error
+    from sklearn.ensemble import GradientBoostingRegressor
+    #
     X = np.random.rand(100, 1) - 0.5
     y = 3 * X[:, 0] ** 2 + 0.05 * np.random.randn(100)
-
-    from sklearn.tree import DecisionTreeRegressor
-
-    tree_reg1 = DecisionTreeRegressor(max_depth=2,random_state=42)
-    tree_reg1.fit(X,y)
-    y2 = y - tree_reg1.predict(X)
-    tree_reg2 = DecisionTreeRegressor(max_depth=2,random_state=42)
-    tree_reg2.fit(X,y2)
-    y3 = y2 - tree_reg2.predict(X)
-    tree_reg3 = DecisionTreeRegressor(max_depth=2,random_state=42)
-    tree_reg3.fit(X,y3)
-    X_new = np.array([[0.8]])
-    y_pred = sum(tree.predict(X_new) for tree in (tree_reg1, tree_reg2, tree_reg3))
-    print(y_pred)
+    X_train, X_val, y_train, y_val = train_test_split(X, y, random_state=42)
+    gbrt = GradientBoostingRegressor(max_depth=2,n_estimators=120)
+    gbrt.fit(X_train,y_train)
+    errors = [mean_squared_error(y_val,y_pred) for y_pred in gbrt.staged_predict(X_val)]
+    bst_n_estimators = np.argmin(errors)
+    gbrt_best = GradientBoostingRegressor(max_depth=2,n_estimators=bst_n_estimators,random_state=42)
+    gbrt_best.fit(X_train,y_train)
+    min_error = np.min(errors)
+    plt.figure(figsize=(11, 4))
+    #
+    # plt.subplot(121)
+    # plt.plot(errors, "b.-")
+    # plt.plot([bst_n_estimators, bst_n_estimators], [0, min_error], "k--")
+    # plt.plot([0, 120], [min_error, min_error], "k--")
+    # plt.plot(bst_n_estimators, min_error, "ko")
+    # plt.text(bst_n_estimators, min_error * 1.2, "Minimum", ha="center", fontsize=14)
+    # plt.axis([0, 120, 0, 0.01])
+    # plt.xlabel("Number of trees")
+    # plt.title("Validation error", fontsize=14)
+    #
+    # plt.subplot(122)
+    # plot_predictions([gbrt_best], X, y, axes=[-0.5, 0.5, -0.1, 0.8])
+    # plt.title("Best model (%d trees)" % bst_n_estimators, fontsize=14)
+    #
+    # plt.show()
 
