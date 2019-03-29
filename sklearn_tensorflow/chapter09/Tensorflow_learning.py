98556da5ecd752f182529ff7e5933f15cc499015
LelandYan <2721599586@qq.com>
2019-03-29 08:29:22 +0800

    linear_regerssion

--- sklearn_tensorflow/chapter09/Tensorflow_learning.py
+++ sklearn_tensorflow/chapter09/Tensorflow_learning.py
@@ -83,11 +83,14 @@ __date__ = '2019/3/28 19:42'
 #         sess.run(training_op)
 #     best_theta = theta.eval()
 import numpy as np
-def my_func(a,b):
+
+
+def my_func(a, b):
     z = 0
     for i in range(100):
-        z = a * np.cos(z+i) + z * np.sin(b-i)
+        z = a * np.cos(z + i) + z * np.sin(b - i)
     return z
+
 
 # a = tf.Variable(0.2,name='a')
 # b = tf.Variable(0.3,name="b")
@@ -110,51 +113,71 @@ def my_func(a,b):
 # print(B_val_1)
 # print(B_val_2)
 
-import numpy as np
-from sklearn.datasets import fetch_california_housing
 import tensorflow as tf
+import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-housing = fetch_california_housing()
-m,n = housing.data.shape
-print(f"数据集:{m}行，{n}列")
-housing_data_plus_bias = np.c_[np.ones((m,1)),housing.data]
-scaler = StandardScaler()
-scaled_housing_data = scaler.fit_transform(housing.data)
-scaled_housing_data_plus_bias = np.c_[np.ones((m,1)),scaled_housing_data]
+housing = pd.read_csv("cal_housing.data", header=None)
+housing_data = housing.iloc[:, :-1].values
+housing_target = housing.iloc[:, -1].values
+m, n = housing_data.shape
+# print(f"数据集:{m}行，{n}列")
+# scaler = StandardScaler()
+# scaled_housing_data = scaler.fit_transform(housing_data)
+# scaled_housing_data_plus_bias = np.c_[np.ones((m,1)),scaled_housing_data]
+#
+# learning_rate = 0.01
+#
+# X = tf.placeholder(tf.float32,shape=(None,n+1),name="X")
+# y = tf.placeholder(tf.float32,shape=(None,1),name="y")
+# theta = tf.Variable(tf.random_uniform([n+1,1],-1.0,1.0,seed=42),name="theta")
+# y_pred = tf.matmul(X,theta,name="predictions")
+# error = y_pred - y
+# mse = tf.reduce_mean(tf.square(error),name="mse")
+# optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+# training_op = optimizer.minimize(mse)
+# #
+# init = tf.global_variables_initializer()
+# n_epochs = 10
+# batch_size = 100
+# n_batches = int(np.ceil(m/batch_size))
+# def fetch_batch(epoch,batch_index,batch_size):
+#     np.random.seed(epoch*n_batches+batch_index)
+#     indices = np.random.randint(m,size=batch_size)
+#     X_batch = scaled_housing_data_plus_bias[indices]
+#     y_batch = housing_target.reshape(-1,1)[indices]
+#     return X_batch,y_batch
+# #
+# with tf.Session() as sess:
+#     sess.run(init)
+#
+#     for epoch in range(n_epochs):
+#         for batch_index in range(n_batches):
+#             X_batch,y_batch = fetch_batch(epoch,batch_index,batch_size)
+#             sess.run(training_op,feed_dict={X:X_batch,y:y_batch})
+#
+#     best_theta = theta.eval()
+# print(best_theta)
 
-n_epochs = 1000
-learning_rate = 0.01
+# Using the normal equation
+housing_data_plus_bias = np.c_[np.ones((m, 1)), housing_data]
+# X = tf.constant(housing_data_plus_bias, dtype=tf.float32, name="X")
+# y = tf.constant(housing_target.reshape(-1, 1), dtype=tf.float32, name='y')
+# XT = tf.transpose(X)
+# theta = tf.matmul(tf.matmul(tf.matrix_inverse(tf.matmul(XT, X)), XT), y)
+# with tf.Session() as sess:
+#     theta_value = theta.eval()
+# print(theta_value)
 
-X = tf.placeholder(tf.float32,shape=(None,n+1),name="X")
-y = tf.placeholder(tf.float32,shape=(None,1),name="y")
-theta = tf.Variable(tf.random_uniform([n+1,1],-1.0,1.0,seed=42),name="theta")
-y_pred = tf.matmul(X,theta,name="predictions")
-error = y_pred - y
-mse = tf.reduce_mean(tf.square(error),name="mse")
-optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
-training_op = optimizer.minimize(mse)
 
-init = tf.global_variables_initializer()
-n_epochs = 10
-batch_size = 100
-n_batches = int(np.ceil(m/batch_size))
-
-def fetch_batch(epoch,batch_index,batch_size):
-    know = np.random.seed(epoch*n_batches+batch_index)
-    print("我是know:",know)
-    indices = np.random.randint(m,size=batch_size)
-    X_batch = scaled_housing_data_plus_bias[indices]
-    y_batch = housing.target.reshape(-1,1)[indices]
-    return X_batch,y_batch
-
-with tf.Session() as sess:
-    sess.run(init)
-
-    for epoch in range(n_epochs):
-        for batch_index in range(n_batches):
-            X_batch,y_batch = fetch_batch(epoch,batch_index,batch_size)
-            sess.run(training_op,feed_dict={X:X_batch,y:y_batch})
-
-    best_theta = theta.eval()
-print(best_theta)
+# compare with pure Numpy
+# X = housing_data_plus_bias
+# y = housing_target.reshape(-1,1)
+# theta_numpy = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
+# print(theta_numpy)
+#
+# # compare with Scikit-Learn
+# from sklearn.linear_model import LinearRegression
+# lin_reg = LinearRegression()
+# lin_reg.fit(housing_data,housing_target.reshape(-1,1))
+# print(np.r_[lin_reg.intercept_.reshape(-1,1),lin_reg.coef_.T])
